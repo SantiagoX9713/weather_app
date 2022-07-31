aa7bb44f65bca07e3206ec80418edf0e4692f777
Santiago Ramos Morales <santiagoramos198@gmail.com>
2022-07-30 23:57:06 -0500

    .gitignore update

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from venv import create
 from app import create_app
 import requests
+import json
 
 
 app = create_app
 
 
-r = requests.get('https://smn.conagua.gob.mx/webservices/?method=1', verify=False)
+r = requests.get('https://smn.conagua.gob.mx/webservices/?method=1')
 print(r.status_code)
-print(r.json())
+print(r.headers)
+# print(r.json())
+# with open('new_file.json', 'w') as f:
+#     json.dump(r.json(), f, indent=2)
